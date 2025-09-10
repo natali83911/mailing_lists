c8669591b4274a6773b45ba8be71783721cccaa6
Natalya Sorokina <natali83911@gmail.com>
2025-09-11 00:24:45 +0300

    19 added files

---
+++ mailing_app/management/commands/sm.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from config.settings import EMAIL_HOST_USER
+from mailing_app.models import Mailing
+from mailing_app.services import send_mailing
+
+
+class Command(BaseCommand):
+    help = "Отправка запущенных рассылок"
+
+    def handle(self, *args, **kwargs):
+        mailings = Mailing.objects.filter(status="Created")
+        if not mailings.exists():
+            self.stdout.write("Нет активных рассылок для отправки.")
+            return
+
+        for mailing in mailings:
+            self.stdout.write(f"Отправка рассылки #{mailing.pk}")
+            send_mailing(mailing, from_email=EMAIL_HOST_USER)
+            self.stdout.write("Готово.")
